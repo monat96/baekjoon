28dc02becc95bd628e07531cba18c99610902c84
monat96 <42468323+monat96@users.noreply.github.com>
2022-02-02 23:24:49 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1629.곱셈/곱셈.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+ll pow(ll a, ll b, ll c)
+{
+    if (b == 1) return a % c;
+    ll val = pow(a, b / 2, c);
+    val = val * val % c;
+    if (b % 2 == 0) return val;
+    return val * a % c;
+}
+
+
+int main(int argc, char const *argv[])
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    ll a, b, c;
+    cin >> a >> b >> c;
+    cout << pow(a, b, c);
+}
