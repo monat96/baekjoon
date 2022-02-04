65923ff9093490694c8443b7a6b24b1eb2019acc
monat96 <42468323+monat96@users.noreply.github.com>
2022-02-05 06:11:32 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652.N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> picked;
+
+void func(int size, int toPick, int pick)
+{
+    if(toPick == pick)
+    {
+        for(auto val : picked) cout << val << ' ';
+        cout << '\n';
+        return ;
+    }
+
+    int start = picked.empty() ? 1 : picked.back();
+
+    for(int next = start; next <= size; next++)
+    {
+        picked.push_back(next);
+        func(size, toPick, pick + 1);
+        picked.pop_back();
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+    func(n, m, 0);
+    
+    return 0;
+}
