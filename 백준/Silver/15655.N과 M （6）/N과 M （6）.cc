c4c179c1d6b6dcb1cee99cb3fb75a2f6e907d434
monat96 <42468323+monat96@users.noreply.github.com>
2022-02-06 02:06:12 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15655.N과 M （6）/N과 M （6）.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int arr[8];
+vector<int> picked;
+
+void func(int size, int toPick, int pick)
+{
+    if(toPick == pick)
+    {
+        for(auto val : picked) cout << arr[val] << ' ';
+        cout << '\n';
+        return ;
+    }
+    int start = picked.empty() ? 0 : picked.back() + 1;
+    
+    for(int next = start; next < size; next++)
+    {
+        picked.push_back(next);
+        func(size, toPick, pick+1);
+        picked.pop_back();
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+
+    for(int i = 0; i < n; i++) cin >> arr[i];
+    sort(arr, arr + n);
+
+    func(n, m, 0);
+    return 0;
+}
