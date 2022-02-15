3b72b5be3562343bd3fb04dfccdb5edee9dca4e2
monat96 <42468323+monat96@users.noreply.github.com>
2022-02-15 19:43:16 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463.1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463.1로 만들기/1로 만들기.cc
@@ -1,35 +1,26 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <string>
-#include <queue>
 
 using namespace std;
+
+int dp[1000001];
 
 int main(int argc, char const *argv[])
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
+    
     int n;
     cin >> n;
-
-    queue<pair<int, int>> que;
-    que.push({n, 0});
-
-    while(true)
+    
+    for(int i = 2; i <= n; i++)
     {
-        pair<int, int> curr = que.front();
-        que.pop();
-
-        int val = curr.first;
-        int cnt = curr.second;
-
-        if(val == 1) {cout << cnt; break;}    
-        
-        if(val % 3 == 0) que.push({val/3, cnt+1});
-        if(val % 2 == 0) que.push({val/2, cnt+1});
-        que.push({val-1, cnt+1});
+        int minimum = dp[i-1];
+        if(i % 3 == 0) minimum = min(minimum, dp[i/3]);
+        if(i % 2 == 0) minimum = min(minimum, dp[i/2]);
+        dp[i] = minimum + 1;
     }
+    cout << dp[n];
     return 0;
 }
