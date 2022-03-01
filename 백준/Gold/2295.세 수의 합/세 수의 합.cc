a2b4458bc010fdde669a904301b2b16852b41fa2
monat96 <42468323+monat96@users.noreply.github.com>
2022-03-02 00:45:50 +0900

    [Gold IV] Title: 세 수의 합, Time: 56 ms, Memory: 8300 KB -BaekjoonHub

---
+++ 백준/Gold/2295.세 수의 합/세 수의 합.cc
@@ -0,0 +1,35 @@
+#include <algorithm>
+#include <iostream>
+#include <numeric>
+#include <set>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int arr[1'001];
+vector<int> two;
+
+int main(int argc, char const* argv[]) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+
+  for (int i = 0; i < n; i++) cin >> arr[i];
+  sort(arr, arr + n);
+
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < n; j++) two.push_back(arr[i] + arr[j]);
+
+  sort(two.begin(), two.end());
+  two.erase(unique(two.begin(), two.end()), two.end());
+
+  for (int i = n - 1; i >= 0; i--)
+    for (int j = 0; j < i; j++)
+      if (binary_search(two.begin(), two.end(), arr[i] - arr[j])) {
+        cout << arr[i];
+        return 0;
+      }
+}
