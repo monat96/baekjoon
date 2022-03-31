a92c77085fab026909191854b8c67777b70ede22
monat96 <42468323+monat96@users.noreply.github.com>
2022-03-31 23:56:58 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string str;
+  cin >> str;
+  str.append("??!");
+  cout << str;
+
+  return 0;
+}
