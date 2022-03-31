ff649be94a16aa8fcdafa1859a2507bd957d91e5
monat96 <42468323+monat96@users.noreply.github.com>
2022-03-31 22:16:56 +0900

    [Silver IV] Title: 베스트셀러, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/1302. 베스트셀러/베스트셀러.cc
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.cc
@@ -2,28 +2,27 @@
 
 using namespace std;
 
-int main(int argc, char const* argv[]) {
+auto comp = [](const pair<string, int> &p1, const pair<string, int> &p2) {
+  if (p1.second == p2.second) return p1.first > p2.first;
+  return p1.second < p2.second;
+};
+
+int main(int argc, char const *argv[]) {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
-  unordered_map<string, int> sale;
+  unordered_map<string, int> li;
 
   int n;
   cin >> n;
 
   while (n--) {
-    string title;
-    cin >> title;
-    sale[title] += 1;
+    string s;
+    cin >> s;
+    li[s] += 1;
   }
-
-  auto pr =
-      max_element(sale.begin(), sale.end(),
-                  [](const pair<string, int>& p1, const pair<string, int>& p2) {
-                    if (p1.second == p2.second) return p1.first > p2.first;
-                    return p1.second < p2.second;
-                  });
-
+  auto pr = max_element(li.begin(), li.end(), comp);
   cout << pr->first;
+
   return 0;
 }
