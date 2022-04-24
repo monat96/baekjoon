6e492076c01506ff621ce801650dcd7e488aae7e
monat96 <42468323+monat96@users.noreply.github.com>
2022-04-24 16:49:19 +0900

    [Gold III] Title: 가장 긴 바이토닉 부분 수열, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.cc
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.cc
@@ -2,30 +2,39 @@
 
 using namespace std;
 
+void func(vector<int> &arr, vector<int> base, int sz) {
+  int tmp[sz];
+  int idx = 1;
+  tmp[0] = base[0];
+  arr[0] = 1;
+  for (int i = 1; i < sz; i++) {
+    if (tmp[idx - 1] < base[i])
+      tmp[idx++] = base[i];
+    else {
+      int ret = lower_bound(tmp, tmp + idx, base[i]) - tmp;
+      tmp[ret] = base[i];
+    }
+    arr[i] = idx;
+  }
+}
+
 int main(int argc, char const *argv[]) {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
   int n;
   cin >> n;
-  vector<int> base(n), lower(n), upper(n);
-  for (auto &ret : base) cin >> ret;
-
-  for (int i = 0; i < n; i++) {
-    upper[i] = 1;
-    for (int j = 0; j < i; j++) {
-      if (base[j] < base[i]) upper[i] = max(upper[i], upper[j] + 1);
-    }
-  }
-
-  for (int i = n - 1; i >= 0; i--) {
-    lower[i] = 1;
-    for (int j = n - 1; j > i; j--) {
-      if (base[j] < base[i]) lower[i] = max(lower[i], lower[j] + 1);
-    }
-  }
+  vector<int> arr(n), lower(n), upper(n);
+  for (auto &ret : arr) cin >> ret;
+  func(lower, arr, n);
+  reverse(arr.begin(), arr.end());
+  func(upper, arr, n);
+  reverse(upper.begin(), upper.end());
   int ans = 0;
-  for (int i = 0; i < n; i++) ans = max(ans, lower[i] + upper[i]);
+  for (int i = 0; i < n; i++) {
+    ans = max(ans, lower[i] + upper[i]);
+  }
   cout << ans - 1;
+
   return 0;
 }
