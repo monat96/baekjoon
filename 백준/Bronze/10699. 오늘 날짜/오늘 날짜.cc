7e9dbed302d4db1e2ee4b01046d3cfbeecafe276
monat96 <42468323+monat96@users.noreply.github.com>
2022-07-19 08:55:59 +0900

    [Bronze V] Title: 오늘 날짜, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout << "2022-07-19" << '\n';
+  return 0;
+}
