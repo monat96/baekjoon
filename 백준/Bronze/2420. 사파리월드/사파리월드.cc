16e924f3b449b4d14a1ef3a5591bd12cbf70e842
monat96 <42468323+monat96@users.noreply.github.com>
2022-07-19 09:55:19 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+int main(int argc, char const* argv[]) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  ll a, b;
+  cin >> a >> b;
+  cout << abs(a - b);
+
+  return 0;
+}
