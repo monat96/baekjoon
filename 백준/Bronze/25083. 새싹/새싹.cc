8375d597e4def95dd9bced8acdad918d1c563e20
monat96 <42468323+monat96@users.noreply.github.com>
2022-07-19 09:52:27 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char const* argv[]) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string ret =
+      "         ,r'\"7 \nr`-_   ,'  ,/\n \\. \". L_r'\n   `~\\/\n      |\n     "
+      " |";
+  cout << ret;
+  return 0;
+}
