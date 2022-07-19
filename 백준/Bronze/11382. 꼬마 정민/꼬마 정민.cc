9c3fbdc2e32377b964299c6c1495094f4ba5d2c7
monat96 <42468323+monat96@users.noreply.github.com>
2022-07-19 09:53:35 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+int main(int argc, char const* argv[]) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  ll a, b, c;
+  cin >> a >> b >> c;
+  cout << a + b + c;
+
+  return 0;
+}
