8ba190cce5094dae06d41db0170f35281ee27e6b
monat96 <42468323+monat96@users.noreply.github.com>
2022-09-26 01:56:52 +0900

    [Bronze V] Title: 이상한 기호, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+int main(int argc, char const* argv[]) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  ll n, m;
+  cin >> n >> m;
+  cout << n * n - m * m;
+
+  return 0;
+}
