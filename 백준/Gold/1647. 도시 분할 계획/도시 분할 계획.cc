cde07737e94fbd09adb325df4e7aa70d0dd2ae76
monat96 <42468323+monat96@users.noreply.github.com>
2024-04-24 01:02:28 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 704 ms, Memory: 44024 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.cc
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+struct compare
+{
+	bool operator()(pair<int, int> a, pair<int, int> b)
+	{
+		return a.second > b.second;
+	}
+};
+
+// idx v, pair<w, c>
+vector<vector<pair<int, int>>> graph(100001);
+bool visited[100001] = {false};
+
+priority_queue<pair<int, int>, vector<pair<int, int>>, compare> pq;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n, m;
+	int v, w, c;
+
+	cin >> n >> m;
+
+	for (int i = 0; i < m; i++)
+	{
+		cin >> v >> w >> c;
+		graph[v].push_back(make_pair(w, c));
+		graph[w].push_back(make_pair(v, c));
+	}
+
+	// start 1
+	while (!graph[1].empty())
+	{
+		pq.push(graph[1].back());
+		graph[1].pop_back();
+	}
+
+	visited[1] = true;
+	int ans = 0;
+	int max = 0;
+
+	while (!pq.empty())
+	{
+		pair<int, int> cur = pq.top();
+		pq.pop();
+
+		if (visited[cur.first])
+		{
+			continue;
+		}
+
+		visited[cur.first] = true;
+		ans += cur.second;
+		if (max < cur.second)
+		{
+			max = cur.second;
+		}
+
+		for (int i = 0; i < graph[cur.first].size(); i++)
+		{
+			if (!visited[graph[cur.first][i].first])
+			{
+				pq.push(graph[cur.first][i]);
+			}
+		}
+	}
+	cout << ans - max << '\n';
+}
